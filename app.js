341ac97f2fb68b161b7ef60482fc8f74d6a986d9
pusheng <sdf753741789@gmail.com>
2022-01-03 16:39:31 +0800

    add showPage and restaurant.JSON

--- app.js
+++ app.js
@@ -2,14 +2,29 @@ const express = require('express')
 const app = express()
 const port = 3000
 const { engine } = require('express-handlebars')
+const restauramtData = require('./restaurant.json')
 
+//靜態文件
 app.use(express.static('public'))
 
+//npm express-handlebars 插件格式
 app.engine('handlebars', engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+//建立首頁路由
 app.get('/', (req, res) => {
-  res.render('index')
+  const restaurant = restauramtData.results
+  res.render('index', { restaurant })
+})
+
+//建立個別詳細頁面路由
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  const restaurantObject = restauramtData.results.find(item => {
+    if (item.id.toString() === id) { return item }
+  })
+
+  res.render('show', { restaurantObject })
 })
 
 app.listen(port, () => {
